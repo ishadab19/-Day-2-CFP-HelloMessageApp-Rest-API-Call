96235aff02c714f6ac43d76438095d75e70cea2c
shadab <me.shadab19@gmail.com>
2022-12-09 23:45:07 +0530

    [Added - UC2 Passing Parameters and using Get and Put methods ]

--- src/main/java/com/messagingapp/hellomessagingapp/controller/HelloRestController.java
+++ src/main/java/com/messagingapp/hellomessagingapp/controller/HelloRestController.java
@@ -1,5 +1,6 @@
 package com.messagingapp.hellomessagingapp.controller;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
+    //UC-1
     @RequestMapping(value = "/hello")
     public String hello() {
         return " Hello From BridgeLabz ";
@@ -14,6 +16,18 @@ public class HelloRestController {
     @RequestMapping(value = "/sayhello",method = RequestMethod.GET)
     public String sayHello(){
         return "My name is Shadab";
+    }
+
+    //UC-2
+
+    @RequestMapping(value = "/query",method = RequestMethod.GET)
+    public String sayHelloAgain(@RequestParam(value = "name")String name){
+        return "Hello " +name+ " From BridgeLabz!!!!";
+    }
+
+    @RequestMapping(value = "/query1",method = RequestMethod.PUT)
+    public String sayHelloAgain(@RequestParam(value = "MobileNumber")long MobileNumber){
+        return "Hello My Mobile Number is ==> " +MobileNumber;
     }
 
 
