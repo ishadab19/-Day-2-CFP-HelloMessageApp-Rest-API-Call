5c7f2563c85c47caf4865de21b95b470d7ea4b50
shadab <me.shadab19@gmail.com>
2022-12-09 23:13:29 +0530

    [Added - UC1 Creating a Rest Controller to demonstarte various Http Method ]

---
+++ src/main/java/com/messagingapp/hellomessagingapp/controller/HelloRestController.java
@@ -0,0 +1,21 @@
+package com.messagingapp.hellomessagingapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    @RequestMapping(value = "/hello")
+    public String hello() {
+        return " Hello From BridgeLabz ";
+    }
+    @RequestMapping(value = "/sayhello",method = RequestMethod.GET)
+    public String sayHello(){
+        return "My name is Shadab";
+    }
+
+
+
+    }
